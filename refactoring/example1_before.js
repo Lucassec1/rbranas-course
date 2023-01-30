814e43e83e987a335a7a9bdc0fc6545647230d17
Lucassec1 <lucasemmanuelcs@gmail.com>
2023-01-30 11:31:34 -0300

    feat: refactoring example

---
+++ refactoring/example1_before.js
@@ -0,0 +1,22 @@
+// calculate ride
+export function calc(dist, d ) {
+
+    // overnight
+
+    if (d.getHours() >= 22) {
+
+        return dist * 3.90;
+
+    } else {
+        // sunday
+        if (d.getDay() === 0) {
+            
+            return dist * 2.90;
+
+        } else {
+            return dist * 2.10;
+
+        }
+    }
+    
+}
