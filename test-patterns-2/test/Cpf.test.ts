dec66bdb3e9eb7f613ce5813e39e8302d8321b7a
Lucassec1 <lucasemmanuelcs@gmail.com>
2023-02-07 10:52:22 -0300

    test: adicinando os testes do frete

--- test-patterns-2/test/Cpf.test.ts
+++ test-patterns-2/test/Cpf.test.ts
@@ -1,22 +1,22 @@
 import Cpf from "../src/Cpf";
 
-test("Should validate a cpf", function () {
+test("Deve validar um cpf", function () {
     const cpf = new Cpf("935.411.347-80");
     expect(cpf).toBeTruthy();
 });
 
-test("Should try validate a invalid cpf", function () {
+test("Deve tentar validar um cpf inválido", function () {
     expect(() => new Cpf("123.456.789-99")).toThrow(new Error("Invalid cpf"));
 });
 
-test("Should try validate a cpf with all digits the same", function () {
+test("Deve tentar validar um cpf com todos os dígitos iguais", function () {
     expect(() => new Cpf("111.111.111-11")).toThrow(new Error("Invalid cpf"));
 });
 
-test("should try to validate a very large invalid cpf", function () {
-    expect(() => new Cpf("123.456.789-1000")).toThrow(new Error("Invalid cpf"));
+test("Deve tentar validar um cpf inválido muito grande", function () {
+	expect(() => new Cpf("123.456.789-1000")).toThrow(new Error("Invalid cpf"));
 });
 
-test("should try to validate a very small invalid cpf", function () {
-    expect(() => new Cpf("123.456")).toThrow(new Error("Invalid cpf"));
+test("Deve tentar validar um cpf inválido muito pequeno", function () {
+	expect(() => new Cpf("123.456")).toThrow(new Error("Invalid cpf"));
 });
