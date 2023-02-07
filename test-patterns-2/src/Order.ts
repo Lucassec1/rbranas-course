27e294dec610da0a922d5c109c19d30cb746f4fe
Lucassec1 <lucasemmanuelcs@gmail.com>
2023-02-07 10:51:26 -0300

    feat: adicionando frete na class order

--- test-patterns-2/src/Order.ts
+++ test-patterns-2/src/Order.ts
@@ -1,34 +1,49 @@
 import Coupon from "./Coupon";
 import Cpf from "./Cpf";
+import DefaultFreightCalculator from "./DefaultFreightCalculator";
+import FreightCalculator from "./FreightCalculator";
 import Item from "./Item";
 import OrderItem from "./OrderItem";
 
 export default class Order {
-    cpf: Cpf;
-    orderItems: OrderItem[]
-    coupon: Coupon | undefined;
+	cpf: Cpf;
+	private orderItems: OrderItem[];
+	coupon: Coupon | undefined;
+	private freight: number;
 
-    constructor (cpf: string) {
-        this.cpf = new Cpf(cpf)
-        this.orderItems = [];
-    }
+	constructor (
+        cpf: string, 
+        readonly date: Date = new Date(), 
+        readonly freightCalculator: FreightCalculator = new DefaultFreightCalculator()
+    ) {
+		this.cpf = new Cpf(cpf);
+		this.orderItems = [];
+		this.freight = 0;
+	}
 
-    addItem(item: Item, quatity: number) {
-        this.orderItems.push(new OrderItem(item.idItem, item.price, quatity))
-    }
+	addItem (item: Item, quantity: number) {
+		this.freight += this.freightCalculator.calculate(item) * quantity;
+		this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+	}
 
-    addCoupon(coupon: Coupon) {
-        this.coupon = coupon;
-    }
+	addCoupon (coupon: Coupon) {
+		if (coupon.isExpired(this.date)) return;
+		this.coupon = coupon;
+	}
 
-    getTotal() {
-        let total = 0;
-        for (const orderItem of this.orderItems) {
-            total += orderItem.getTotal();
-        }
-        if (this.coupon) {
-            total -= (total * this.coupon.percentage) / 100;
-        }
-        return total;
-    }
+	getFreight () {
+		return this.freight;
+	}
+
+	getTotal () {
+		let total = 0;
+		for (const orderItem of this.orderItems) {
+			total += orderItem.getTotal();
+		}
+		if (this.coupon) {
+			total -= this.coupon.calculateDiscount(total, this.date);
+		}
+		total += this.getFreight();
+		return total;
+	}
 }
